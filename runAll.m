9d2d1ee7d8122648ae26cdaaced0b70c25ac51d8
bartkrekelberg <bart@vision.rutgers.edu>
2025-07-11 08:04:34 +0200

    runAll improvements.

--- runAll.m
+++ runAll.m
@@ -1,42 +1,49 @@
-function runAll(trgFoldr, exportFormats,mlxFiles)
+function runAll(pv)
 %% Run all mlx files
 % This is intended as a test that all code runs. This will not update the
 % MLX files but does generate regular figures, exports them as pnd and pdf
 % and the command line output from the mlx files is stored ina a txt file.  
 % This output is saved to the trgFolder.
 arguments
-    trgFoldr    (1,1) string    = "c:/temp/artsim/";
-    exportFormats  (1,:) string  = ["-dpng" "-dpdf"];
-    mlxFiles      struct = dir('*.mlx');
+    pv.trgFoldr    (1,1) string    = "c:/temp/artsim/";
+    pv.exportFormats  (1,:) string  = ["-dpng" "-dpdf"];
+    pv.mlxFiles      struct = dir('*.mlx');
+    pv.overwrite (1,1) logical = false
 end
 
 close all;
-if ~exist(trgFoldr,"dir")
-    mkdir(trgFoldr);
+if ~exist(pv.trgFoldr,"dir")
+    mkdir(pv.trgFoldr);
 end
-for i=1:numel(mlxFiles)
+for i=1:numel(pv.mlxFiles)
     try
-        clearvars -except mlxFiles i trgFoldr exportFormats % Avoid out of memory / scope issues        
-        name = extractBefore(mlxFiles(i).name,'.mlx');
-        fprintf(2,'**************** %s ****************\n',name)
-        diary(fullfile(trgFoldr,[name '.txt']))
+        clearvars -except pv i
+        name = extractBefore(pv.mlxFiles(i).name,'.mlx');
+         trgFile = fullfile(pv.trgFoldr,name);
+        if ~exist(trgFile + ".mat" ,'file') || pv.overwrite 
+             fprintf(2,'**************** %s ****************\n',name)      
+            diary(trgFile + ".txt");
         tic
-        run(mlxFiles(i).name)
+        run(pv.mlxFiles(i).name)
         figures = findobj(0,'type','figure');
-        savefig(fig, fullfile(trgFoldr,extractBefore(mlxFiles(i).name,'.mlx'))); % All figures in one .fig file
-        for exportFormat = exportFormats 
+        savefig(figures, trgFile); % All figures in one .fig file
+        for exportFormat = pv.exportFormats 
             for fig =figures'
-                filename = fullfile(trgFoldr,[name get(fig,'Name')]);
+                filename = fullfile(pv.trgFoldr,[name get(fig,'Name')]);
                 print(fig,exportFormat,filename);
             end
         end
-        close(figures)
+        close all 
         clear figures
-        save(fullfile(trgFoldr,name)) % Save state to .mat file nanmed after mlx
+        save(trgFile) % Save state to .mat file nanmed after mlx        
         toc
         diary off
+        else
+             fprintf(2,'**************** %s (Skipped) *******\n',name)      
+        end
     catch me
-        fprintf(2,'\n  !!!! %s  !!!! \n',me.message)
+        fprintf(2,'\n %s: !!!! %s  !!!! \n',name, me.message)
+        me.stack
         close all
         diary off
     end
